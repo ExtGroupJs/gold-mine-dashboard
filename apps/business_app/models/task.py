8b72ec9649581e95f5092ebe89e9a7b0e19c9d16
Enrique Almeida <ealmeida@example.com>
2025-12-24 16:07:15 -0500

    include headers

--- apps/business_app/models/task.py
+++ apps/business_app/models/task.py
@@ -6,19 +6,19 @@ from django.utils.translation import gettext_lazy as _
 
 
 class Task(models.Model):
-    task_code = models.CharField(max_length=50, unique=True)
-    status_code = models.CharField(max_length=50)
-    wbs = models.ForeignKey(WBS, on_delete=models.DO_NOTHING)
-    task_name = models.TextField()
-    target_drtn_hr_cnt = models.IntegerField()
+    task_code = models.CharField(max_length=50, unique=True, verbose_name="Activity ID")
+    status_code = models.CharField(max_length=50, verbose_name="Activity Status")
+    wbs = models.ForeignKey(WBS, on_delete=models.DO_NOTHING, verbose_name="WBS Code")
+    task_name = models.TextField(verbose_name="Activity Name")
+    target_drtn_hr_cnt = models.IntegerField(verbose_name="Original Duration(d)")
     # original_duration = models.IntegerField()
-    remain_drtn_hr_cnt = models.IntegerField()
-    start_date = models.DateTimeField(null=True, blank=True)
-    end_date = models.DateTimeField(null=True, blank=True)
-    target_cost = models.DecimalField(max_digits=10, decimal_places=2)
-    total_float_hr_cnt = models.IntegerField()
-    delete_record_flag = models.BooleanField(default=False)
-    resources = models.ManyToManyField(Resource, through=TaskResource)
+    remain_drtn_hr_cnt = models.IntegerField(verbose_name="Remaining Duration(d)")
+    start_date = models.DateTimeField(null=True, blank=True, verbose_name="(*)Start")
+    end_date = models.DateTimeField(null=True, blank=True, verbose_name="(*)Finish")
+    target_cost = models.DecimalField(max_digits=10, decimal_places=2, verbose_name="(*)Budgeted Total Cost($)")
+    total_float_hr_cnt = models.IntegerField(verbose_name="(*)Total Float(d)")
+    delete_record_flag = models.BooleanField(default=False, verbose_name="Delete This Row")
+    resources = models.ManyToManyField(Resource, through=TaskResource, verbose_name="(*)Resources")
 
     class Meta:
         verbose_name = _("Task")
