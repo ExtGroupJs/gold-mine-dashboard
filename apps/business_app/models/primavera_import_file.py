56376dd26d082131ade3cd36eaa150028f6cfa04
Enrique Almeida <ealmeida@example.com>
2025-12-23 19:58:42 -0500

    removed phisical file when deleting primavera_import_file

--- apps/business_app/models/primavera_import_file.py
+++ apps/business_app/models/primavera_import_file.py
@@ -4,6 +4,7 @@ from django.utils.deconstruct import deconstructible
 from .allowed_extensions import AllowedExtensions
 import os
 from django.core.exceptions import ValidationError
+from apps.users_app.models.system_user import SystemUser
 
 
 
@@ -22,7 +23,7 @@ class PrimaveraImportFile(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     extra_info = models.TextField(blank=True, null=True)
     system_user = models.ForeignKey(
-        "users_app.SystemUser",
+        SystemUser,
         on_delete=models.CASCADE,
         related_name="uploaded_files",
     )
@@ -32,6 +33,18 @@ class PrimaveraImportFile(models.Model):
 
     def __str__(self):
         return self.file.name
+
+    def delete(self, *args, **kwargs):
+        # Delete the physical file before deleting the record
+        self.delete_physical_file(self.file)
+        super().delete(*args, **kwargs)
+
+    def delete_physical_file(self, file_field):
+        if file_field:
+            file_path = file_field.path
+            if os.path.exists(file_path):
+                os.remove(file_path)
+
 
     # def save(self, *args, **kwargs):
     #     original_file = self.original_file
@@ -77,22 +90,3 @@ class PrimaveraImportFile(models.Model):
     #         UploadedFiles.objects.filter(gene=self.gene).exclude(id=self.id).update(
     #             predefined=False
     #         )
-
-    # def delete(self, *args, **kwargs):
-    #     # Delete the physical file before deleting the record
-    #     self.delete_physical_file(self.original_file)
-    #     if self.google_sheet_id:
-    #         processor = UploadToGoogleDriveApi()
-    #         processor.delete_file_from_google_drive(self.google_sheet_id)
-    #     cache.delete(
-    #         UploadedFiles.CACHE_KEY_RELATED_ALLELE_NODES.format(
-    #             uploaded_file_id=self.id
-    #         )
-    #     )
-    #     super().delete(*args, **kwargs)
-
-    # def delete_physical_file(self, file_field):
-    #     if file_field:
-    #         file_path = file_field.path
-    #         if os.path.exists(file_path):
-    #             os.remove(file_path)
