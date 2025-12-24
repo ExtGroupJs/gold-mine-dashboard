ba7d0c01560921c1afa883bdcc1fe4a9ff1e3b5e
Enrique Almeida <ealmeida@example.com>
2025-12-24 16:03:15 -0500

    created task endpoints

---
+++ apps/business_app/serializers/task.py
@@ -0,0 +1,8 @@
+from rest_framework import serializers
+from ..models.task import Task
+class TaskSerializer(serializers.ModelSerializer):
+    wbs = serializers.StringRelatedField()
+    resources = serializers.StringRelatedField(many=True)
+    class Meta:
+        model = Task
+        fields = "__all__"
