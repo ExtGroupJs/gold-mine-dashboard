e2e4755bd8c998f4a6f1ccaa79a3710074b165b5
Enrique Almeida <ealmeida@example.com>
2025-12-30 08:57:15 -0500

    added comment

--- apps/users_app/models/groups.py
+++ apps/users_app/models/groups.py
@@ -3,6 +3,7 @@ from django.utils.translation import gettext_lazy as _
 
 
 class Groups(models.IntegerChoices):
+    r"""Update apps\users_app\fixtures\auth.group.json and run python manage.py load_data"""
     # ** Administrativo
     SUPER_ADMIN = 1, _("Super Admin")
     PLANNER = 2, _("Planner")
