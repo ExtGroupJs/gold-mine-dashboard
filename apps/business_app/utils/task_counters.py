a3ff7f7ae54f7e732b1a5523a1b979eacbe40eb5
Enrique Almeida <ealmeida@example.com>
2025-12-31 01:15:06 -0500

    updated counter for dashboard

--- apps/business_app/utils/task_counters.py
+++ apps/business_app/utils/task_counters.py
@@ -1,10 +1,24 @@
 from ..models.task import Task
+from ..models.alert import Alert
+
 
 
 def get_task_counters():
     resp = {}
+    task_info = {}
     task_queryset = Task.objects.all()
+
     for status in Task.INTERNAL_STATUS:  # status es un miembro del enum
-        resp[str(status.label)] = task_queryset.filter(internal_status=status).count()
-    resp["total"] = task_queryset.count()
+        task_info[str(status.label)] = task_queryset.filter(internal_status=status).count()
+    task_info["total"] = task_queryset.count()
+    resp["task_info"] = task_info
+    
+    alert_info = {}
+    alert_queryset = Alert.objects.all()
+
+    for kind in Alert.KIND:  # status es un miembro del enum
+        alert_info[str(kind.label)] = alert_queryset.filter(kind=kind).count()
+    alert_info["total"] = alert_queryset.count()
+    resp["alert_info"] = alert_info
     return resp
+
