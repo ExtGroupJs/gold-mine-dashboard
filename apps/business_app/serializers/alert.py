5a8fbc4d664e1a85e4718275ba3fcf47e0377d86
Enrique Almeida <ealmeida@example.com>
2025-12-30 20:21:35 -0500

    improved code

--- apps/business_app/serializers/alert.py
+++ apps/business_app/serializers/alert.py
@@ -18,31 +18,14 @@ class AlertSerializer(serializers.ModelSerializer):
         ]
         read_only_fields = ["created_at", "updated_at"]
 
-    def save(self, **kwargs):
-        validated_data = {**self.validated_data, **kwargs}
-        if not self.instance:
-            self.instance = ShopProducts.objects.create(**validated_data)
-        if "alert_type" in validated_data:
-            if validated_data["alert_type"] == Alert.KIND.CRITICAL:
-                self.instance.task.internal_status = Task.INTERNAL_STATUS.HOLD
-            else:
-                self.instance.task.internal_status = Task.INTERNAL_STATUS.IN_PROGRESS
-            self.instance.task.save(update_fields=["internal_status"])
-        return super().save(**kwargs)
-
     def update_task_internal_status(self, alert):
         task = alert.task
         if task.internal_status == Task.INTERNAL_STATUS.COMPLETED:
             return
-        if alert.kind == Alert.KIND.CRITICAL:
+        if Alert.objects.filter(task=task, kind=Alert.KIND.CRITICAL).exists():
             task.internal_status = Task.INTERNAL_STATUS.HOLD
         else:
-            if Task.objects.filter(
-                id=task.id, alert__kind=Alert.KIND.CRITICAL
-            ).exists():
-                task.internal_status = Task.INTERNAL_STATUS.HOLD
-            else:
-                task.internal_status = Task.INTERNAL_STATUS.IN_PROGRESS
+            task.internal_status = Task.INTERNAL_STATUS.IN_PROGRESS
         task.save(update_fields=["internal_status"])
 
     def create(self, validated_data):
--- apps/business_app/views/alert.py
+++ apps/business_app/views/alert.py
@@ -4,12 +4,14 @@ from rest_framework.generics import GenericAPIView
 from rest_framework import permissions
 
 from ..models.alert import Alert
+from ..models.task import Task
 from ..serializers.alert import AlertSerializer
 from apps.common.mixins.common_view_mixin import CommonOrderingFilter
+from rest_framework.response import Response
+from rest_framework import status
 
 
 # Create your views here.
-from apps.common.permissions import IsAuthenticatedAndReadOnly
 
 
 class AlertViewSet(viewsets.ModelViewSet, GenericAPIView):
@@ -35,3 +37,14 @@ class AlertViewSet(viewsets.ModelViewSet, GenericAPIView):
         "task__wbs__id": ["exact", "in"],
         "task__resources__id": ["exact", "in"],
     }
+
+    def destroy(self, request, *args, **kwargs):
+        instance = self.get_object()
+        task = instance.task
+        self.perform_destroy(instance)
+        if Alert.objects.filter(task=task, kind=Alert.KIND.CRITICAL).exists():
+            task.internal_status = Task.INTERNAL_STATUS.HOLD
+        else:
+            task.internal_status = Task.INTERNAL_STATUS.IN_PROGRESS
+        task.save(update_fields=["internal_status"])
+        return Response(status=status.HTTP_204_NO_CONTENT)
