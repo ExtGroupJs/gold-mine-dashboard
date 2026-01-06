56219c34c5d53c7910ec6070d826e1d9f098eb54
ealmeida <ealmeida.cu@gmail.com>
2026-01-06 15:08:31 -0500

    updated rules

--- apps/business_app/serializers/task.py
+++ apps/business_app/serializers/task.py
@@ -61,23 +61,22 @@ class TaskSerializer(serializers.ModelSerializer):
         return data
 
     def update(self, instance, validated_data):
-        if (
-            not instance.internal_planned_date
-            and "internal_planned_date" in validated_data
-        ):
+        if "internal_planned_date" in validated_data:
             validated_data["internal_status"] = Task.INTERNAL_STATUS.PLANNED
-        if "internal_percent_complete" in validated_data:
+        
+        elif "internal_percent_complete" in validated_data:
             if validated_data.get("internal_percent_complete") == 100:
                 validated_data["internal_status"] = Task.INTERNAL_STATUS.COMPLETED
             elif validated_data.get("internal_percent_complete") != 0:
                 validated_data["act_start_date"] = datetime.now()
             validated_data["complete_pct"] = validated_data.get("internal_percent_complete")
 
-        if "act_end_date" in validated_data:
+        elif "act_end_date" in validated_data:
             validated_data["internal_status"] = Task.INTERNAL_STATUS.COMPLETED
             validated_data["internal_percent_complete"] = 100
             validated_data["complete_pct"] = 100
             Alert.objects.filter(task=instance).delete()
+        
         if "act_start_date" in validated_data:
             validated_data["internal_status"] = Task.INTERNAL_STATUS.IN_PROGRESS
 
