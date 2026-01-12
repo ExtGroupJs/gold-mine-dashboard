0dd34a8a36e769f443bd03ee92075329b78c8d47
Enrique Almeida <ealmeida@example.com>
2026-01-12 12:06:05 -0500

    created model for Resource Owner

--- apps/business_app/models/resource.py
+++ apps/business_app/models/resource.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.utils.translation import gettext_lazy as _
+from ..models.resource_owner import ResourceOwner
 
 
 class Resource(models.Model):
@@ -9,6 +10,13 @@ class Resource(models.Model):
     rent_cost_by_hour_in_euros = models.FloatField(default=0)
     processed_volume_by_hour = models.FloatField(default=0)
     processed_area_by_hour = models.FloatField(default=0)
+    owner = models.ForeignKey(
+        to=ResourceOwner,
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        related_name="resources",
+    )
 
     # cost_per_hour = models.DecimalField(max_digits=10, decimal_places=2)
     class Meta:
